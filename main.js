8d66b3b0489ab09a9abab03762848aa815c51776
PixelGH <32110365+PixelGH@users.noreply.github.com>
2019-03-03 19:33:57 +0100

    Zeug 2

--- main.js
+++ main.js
@@ -5,12 +5,16 @@ const {app, BrowserWindow} = require('electron')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+global.sharedObject = {
+  cat: 'random'
+}
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadFile('_itch-list.html')
+  mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
